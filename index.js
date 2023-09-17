271eef8b281d4565baffe7c45195743c12142b00
Ronny Chamba <ronnychamba96@gmail.com>
2023-09-17 13:54:21 -0500

    add operation division

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ let sum = numberOne + numberTwo;
 console.log(sum)
 
 console.log("Esta es un nuevo cambio")
+
+
+let numeroDos = 12;
+
+let numberTres = 6;
+
+let division = numeroDos / numberTres;
+
+console.log(division);
